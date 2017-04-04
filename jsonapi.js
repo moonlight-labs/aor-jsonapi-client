bac2bb38698efd2c4c768369d37914df06486f6a
Max J. Schridde <maxschridde@berkeley.edu>
2017-04-04 13:54:07 -0700

    Baseline implementation of convertRESTRequestToHTTP and convertHTTPResponseToRest. Does not touch filtering (for GET_LIST or GET_MANY) or GET_MANY_REFERENCE).

--- jsonapi.js
+++ jsonapi.js
@@ -24,26 +24,46 @@ import {
         let url = '';
         const options = {};
         switch (type) {
-        case GET_LIST: 
-
+        case GET_LIST:
+            //don't deal with filter, assume sort only gets one field specified for now
+            const { page, perPage } = params.pagination;
+            const { field, order } = params.sort;
+            var query = {
+                'page[offset]': (page - 1) * perPage,
+                'page[limit]': perPage,
+            };
+            if (order === 'ASC'){
+                query.sort = field;
+            }else{
+                query.sort = '-' + field;
+            }
+            url = `${apiUrl}/${resource}?${queryParameters(query)}`;
             break;
         case GET_ONE:
-
+            url = `${apiUrl}/${resource}/${params.id}`;
             break;
-        case GET_MANY: 
-
+        case GET_MANY:
+            // const ids = params.ids;
+            // const query = {filter: ids.toString()};
+            // url = `${apiUrl}/${resource}?${queryParameters(query)}`;
             break;
         case GET_MANY_REFERENCE: 
-
             break;
         case UPDATE:
-
+            url = `${apiUrl}/${resource}/${params.id}`;
+            options.method = 'PATCH';
+            const updateParams = {type: resource, id: params.id, attributes: params.data}
+            options.body = JSON.stringify(updateParams);
             break;
         case CREATE:
-
+            url = `${apiUrl}/${resource}`;
+            options.method = 'PUT';
+            const createParams = {type: resource, attributes: params.data, }
+            options.body = JSON.stringify(createParams);
             break;
         case DELETE:
-
+            url = `${apiUrl}/${resource}/${params.id}`;
+            options.method = 'DELETE';
             break;
         default:
             throw new Error(`Unsupported fetch action type ${type}`);
@@ -60,30 +80,21 @@ import {
      */
     const convertHTTPResponseToREST = (response, type, resource, params) => {
         const { headers, json } = response;
+        console.log(headers);
         switch (type) {
         case GET_LIST: 
-
-            return ;
-        case GET_ONE:
-
-            return ;
-        case GET_MANY: 
-
-            return ;
+            // incorrect way of calculating total. 
+            return {data: json.data, total: json.data.length};
         case GET_MANY_REFERENCE: 
-
             return ;
         case UPDATE:
-
-            return ;
         case CREATE:
-
-            return ;
+            return { data: Object.assign({id: json.data.id}, json.data.attributes) };
         case DELETE:
-
-            return ;
+            //does JSON API return the deleted object?
+            return { data: json};
         default:
-            throw new Error(`Unsupported fetch action type ${type}`);
+            return {data:json.data};
         }
     };
 
@@ -99,4 +110,3 @@ import {
             .then(response => convertHTTPResponseToREST(response, type, resource, params));
     };
 };
-
